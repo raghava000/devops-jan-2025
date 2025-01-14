e25005a3f5a4f4bc7fc88a9ce1d8012d0cb47e13
raghava <raghava000@gmail.com>
2025-01-14 16:33:58 +0530

    initial commit

---
+++ devops-jan-2025/src/main/java/com/devops/devops_jan_2025/DevopsJan2025Application.java
@@ -0,0 +1,19 @@
+package com.devops.devops_jan_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsJan2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJan2025Application.class, args);
+	}
+	 @GetMapping("/")
+	    public String hello() {
+	      return String.format("<h1> Hello There, Welcome to my project <h1>");
+}
+}
---
+++ devops-jan-2025/src/test/java/com/devops/devops_jan_2025/DevopsJan2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_jan_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJan2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
